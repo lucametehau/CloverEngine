48ccc918a541b5b7157d96d5eb57428a4d6bc0df
lucametehau <lucametehau@gmail.com>
2021-06-10 22:37:26 +0300

    upd version

--- uci.h
+++ uci.h
@@ -33,7 +33,7 @@
 /// is this working?
 /// i guess so?
 
-const std::string VERSION = "2.4-dev21"; /// 2.0 was "FM"
+const std::string VERSION = "2.4-dev22"; /// 2.0 was "FM"
 
 char line[INPUTBUFFER];
 
