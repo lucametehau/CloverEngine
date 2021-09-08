bc5a583610e8ab59d50a615008c3103670902c99
lucametehau <lucametehau@gmail.com>
2021-09-08 14:03:44 +0300

    .

--- generate.h
+++ generate.h
@@ -12,7 +12,7 @@ struct FenData {
 
 void generateFens(int id, int nrFens, std::string path) {
   std::ofstream out (path);
-  std::mt19937_64 gn(getTime() * id + 5873258LL);
+  std::mt19937_64 gn(getTime() * id + 74628599LL);
   std::uniform_int_distribution <uint32_t> rnd;
 
   Info info[1];
@@ -129,7 +129,7 @@ void generateData(int nrFens, int nrThreads) {
   srand(time(0));
 
   for(int i = 0; i < nrThreads; i++) {
-    path[i] = "CloverData6.";
+    path[i] = "CloverData7.";
     path[i] += char(i + '0');
     path[i] += ".txt";
     std::cout << path[i] << "\n";
