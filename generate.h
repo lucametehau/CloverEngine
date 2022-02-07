daca849c2ee9939d582a80c33422a6e36c8ea4d4
lucametehau <lucametehau@gmail.com>
2022-02-07 18:44:24 +0200

    a

--- generate.h
+++ generate.h
@@ -30,7 +30,8 @@ struct FenData {
 
 void generateFens(std::atomic <int>& sumFens, int nrFens, std::string path, uint64_t seed) {
     std::ofstream out(path);
-    std::mt19937_64 gn(seed);
+    std::mt19937_64 gn(seed + 9367376037LL);
+    std::mt19937_64 gn(seed + 9367376037LL);
 
     Info info[1];
     int gameInd = 1, totalFens = 0;
