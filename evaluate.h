3984e184e1861bd0ce1d4859e29842c5cb25c18b
lucametehau <31002793+lucametehau@users.noreply.github.com>
2022-05-12 11:26:38 +0300

    another try on phase scaling
    Bench: 2957981

--- evaluate.h
+++ evaluate.h
@@ -23,13 +23,23 @@
 
 const int TEMPO = 20;
 
+const int phaseVal[] = { 0, 0, 1, 1, 2, 4 };
+const int maxPhase = 16 * phaseVal[PAWN] + 4 * phaseVal[KNIGHT] + 4 * phaseVal[BISHOP] + 4 * phaseVal[ROOK] + 2 * phaseVal[QUEEN];
+
 int evaluate(Board &board) {
     //board.print();
     int eval = int(board.NN.getOutput());
 
     bool turn = board.turn;
+    int phase = count(board.bb[WP] | board.bb[BP]) * phaseVal[PAWN] +
+        count(board.bb[WN] | board.bb[BN]) * phaseVal[KNIGHT] +
+        count(board.bb[WB] | board.bb[BB]) * phaseVal[BISHOP] +
+        count(board.bb[WR] | board.bb[BR]) * phaseVal[ROOK] +
+        count(board.bb[WQ] | board.bb[BQ]) * phaseVal[QUEEN];
 
     //eval += contempt;
+
+    eval = eval * (maxPhase / 2 + phase) / maxPhase;
 
     return eval * (turn == WHITE ? 1 : -1) + TEMPO;
 }
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 CC  = g++
 SRC = *.cpp tbprobe.c
-EXE = Clover.3.2-dev7
+EXE = Clover.3.2-dev8
 EVALFILE = Clover_3_1_590mil_e32_512_eval.nn
 
 ifeq ($(OS), Windows_NT)
