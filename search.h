69c3279e6bf3f8b5495314bd97e6377cf8e47154
lucametehau <31002793+lucametehau@users.noreply.github.com>
2022-12-09 22:24:59 +0200

    meh
    Bench: 3061276

--- search.h
+++ search.h
@@ -564,14 +564,11 @@ int Search::search(int alpha, int beta, int depth, bool cutNode, uint16_t exclud
         /// principal variation search
 
         uint64_t initNodes = nodes;
-        bool interesting = false, updMoveHist = false;
+        bool interesting = false;
 
         if (R != 1) {
             score = -search(-alpha - 1, -alpha, newDepth - R, true);
             interesting = (score > alpha + 50);
-
-            if (score > alpha && isQuiet)
-                updMoveHist = true;
         }
 
         if ((R != 1 && score > alpha) || (R == 1 && (!pvNode || played > 1))) {
@@ -585,9 +582,6 @@ int Search::search(int alpha, int beta, int depth, bool cutNode, uint16_t exclud
         undoMove(board, move);
 
         nodesSearched[sqFrom(move)][sqTo(move)] += nodes - initNodes;
-
-        if (updMoveHist)
-            updateMoveHistory(this, move, ply, getHistoryBonus(depth) / 2);
 
         if (flag & TERMINATED_SEARCH) /// stop search
             return ABORT;
