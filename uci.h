3a6b2b58e4434c67180115ad0004d66d42be71fe
lucametehau <31002793+lucametehau@users.noreply.github.com>
2022-12-14 20:31:13 +0200

    reduce less for moves that give check try 2
    Bench: 3030892

--- uci.h
+++ uci.h
@@ -26,7 +26,7 @@
 #include "generate.h"
 #include "tune.h"
 
-const std::string VERSION = "3.2.1"; /// 2.0 was "FM"
+const std::string VERSION = "3.3-dev2"; /// 2.0 was "FM"
 
 struct Option {
     std::string name;
