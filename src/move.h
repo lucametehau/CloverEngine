36e720f66cc34355eaac4008f8253f17d76081a6
lucametehau <31002793+lucametehau@users.noreply.github.com>
2023-04-03 07:54:39 +0300

    hopefully illegal move bug fix and new net
    Bench: 3764111

--- src/move.h
+++ src/move.h
@@ -335,11 +335,11 @@ void Board::makeNullMove() {
     key ^= (enPas >= 0 ? enPasKey[enPas] : 0);
 
     captured = 0;
-    checkers = 0;
     enPas = -1;
     turn ^= 1;
     key ^= 1;
     pinnedPieces = getPinnedPieces(*this, turn);
+    checkers = getAttackers(*this, turn, pieces[WHITE] | pieces[BLACK], king(1 ^ turn));
     ply++;
     gamePly++;
     halfMoves++;
