cac25cd8a822329910684f92a27b3bdcde64c95e
lucametehau <31002793+lucametehau@users.noreply.github.com>
2023-04-03 21:07:23 +0300

    limit cause why not

--- src/uci.h
+++ src/uci.h
@@ -260,7 +260,7 @@ void UCI::Uci_Loop() {
 
             info->nodes = nodes;
             if (depth == -1)
-                info->depth = DEPTH;
+                info->depth = 15;
 
             Go(info);
         }
