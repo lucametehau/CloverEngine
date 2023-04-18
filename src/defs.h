41f8c6832958e467bc322db0fbd479dabd978059
lucametehau <31002793+lucametehau@users.noreply.github.com>
2023-04-18 20:00:21 +0300

    final fix hopefully?
    Bench: 3458523

--- src/defs.h
+++ src/defs.h
@@ -15,6 +15,9 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #pragma once
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
 #include <string>
 #include <chrono>
 #include <ctime>
