9ef40e3aa3cf3cd6e26b4ee1edaae98d3c5fd920
lucametehau <31002793+lucametehau@users.noreply.github.com>
2023-05-29 20:55:44 +0300

    speedup
    Bench: 3661863

--- src/movepick.h
+++ src/movepick.h
@@ -74,12 +74,6 @@ public:
             trueStage = STAGE_HASHMOVE;
             stage++;
 
-            if (isLegalMove(board, hashMove) != isLegalMoveSlow(board, hashMove)) {
-                board.print();
-                std::cout << toString(hashMove) << " " << isLegalMove(board, hashMove) << "\n";
-                exit(0);
-            }
-
             if (hashMove && isLegalMove(board, hashMove)) {
                 return hashMove;
             }
