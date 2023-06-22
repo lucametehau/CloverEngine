284d307baaf60a48b982cdc218a51151e69a62db
lucametehau <31002793+lucametehau@users.noreply.github.com>
2023-06-23 01:10:44 +0300

    Bench: 3216148

--- src/search.h
+++ src/search.h
@@ -452,7 +452,7 @@ int Search::search(int alpha, int beta, int depth, bool cutNode, StackEntry* sta
             }
 
             // probcut
-            int probBeta = beta + 200 - 50 * improving;
+            int probBeta = beta + 200;
             if (depth >= 5 && abs(beta) < MATE && !(ttHit && entry.depth() >= depth - 3 && ttValue < probBeta)) {
                 Movepick picker((ttMove && isNoisyMove(board, ttMove) && see(board, ttMove, probBeta - staticEval) ? ttMove : NULLMOVE),
                     NULLMOVE,
