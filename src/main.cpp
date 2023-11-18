a63fe5b99f5e03b6b670068330a61fc18d917427
lucametehau <31002793+lucametehau@users.noreply.github.com>
2023-11-18 21:11:58 +0100

    Bench: 3176882

--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,18 @@ int main(int argc, char** argv) {
             uci.Bench(depth);
             return 0;
         }
+        if (!strncmp(argv[1], "generate", 8)) {
+            if (argc < 5) {
+                std::cout << "Wrong number of arguments!\n";
+                return 0;
+            }
+            uint64_t nrFens = stoull(std::string(argv[2])), nrThreads = stoi(std::string(argv[3])), extraSeed = 0;
+            std::string rootPath = std::string(argv[4]);
+            if (argc == 6)
+                extraSeed = stoull(std::string(argv[5]));
+            generateData(nrFens, nrThreads, rootPath, extraSeed);
+            return 0;
+        }
     }
 
     uci.uciLoop();
