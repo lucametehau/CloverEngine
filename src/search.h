c03f98e0fc4f582a82d7bfc085c026fceda0543d
lucametehau <31002793+lucametehau@users.noreply.github.com>
2024-03-01 09:51:10 +0100

    Bench: 3640934

--- src/search.h
+++ src/search.h
@@ -591,6 +591,7 @@ int Search::search(int alpha, int beta, int depth, bool cutNode, StackEntry* sta
             }
 
             R += 2 * cutNode;
+            R -= wasPV && ttDepth >= depth;
 
             R = std::min(newDepth, std::max(R, 1)); /// clamp R
             score = -search<false>(-alpha - 1, -alpha, newDepth - R, true, stack + 1);
