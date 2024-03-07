e7111123b4c1dbd912746dd18a6f23f5f3bd97cc
lucametehau <31002793+lucametehau@users.noreply.github.com>
2024-03-07 23:02:59 +0100

    Bench: 2723421

--- src/search.h
+++ src/search.h
@@ -534,19 +534,22 @@ int Search::search(int alpha, int beta, int depth, bool cutNode, StackEntry* sta
 #endif
 
         int ex = 0;
-        /// singular extension (look if the tt move is better than the rest)
-        if (!stack->excluded && !allNode && move == ttMove && abs(ttValue) < MATE && depth >= SEDepth && ttDepth >= depth - 3 && (bound & LOWER)) {
-            int rBeta = ttValue - SEMargin * depth / 64;
+        /// avoid extending too far (might cause stack overflow)
+        if (ply < 2 * tDepth) {
+            /// singular extension (look if the tt move is better than the rest)
+            if (!stack->excluded && !allNode && move == ttMove && abs(ttValue) < MATE && depth >= SEDepth && ttDepth >= depth - 3 && (bound & LOWER)) {
+                int rBeta = ttValue - SEMargin * depth / 64;
 
-            stack->excluded = move;
-            int score = search<false>(rBeta - 1, rBeta, (depth - 1) / 2, cutNode, stack);
-            stack->excluded = NULLMOVE;
+                stack->excluded = move;
+                int score = search<false>(rBeta - 1, rBeta, (depth - 1) / 2, cutNode, stack);
+                stack->excluded = NULLMOVE;
 
-            if (score < rBeta) ex = 1 + (!pvNode && rBeta - score > SEDoubleExtensionsMargin);
-            else if (rBeta >= beta) return rBeta; // multicut
-            else if (ttValue >= beta || ttValue <= alpha) ex = -1 - !pvNode;
+                if (score < rBeta) ex = 1 + (!pvNode && rBeta - score > SEDoubleExtensionsMargin);
+                else if (rBeta >= beta) return rBeta; // multicut
+                else if (ttValue >= beta || ttValue <= alpha) ex = -1 - !pvNode;
+            }
+            else if (isCheck) ex = 1;
         }
-        else if (isCheck) ex = 1;
         else if (allNode && played >= 1 && ttDepth >= depth - 3 && bound == UPPER) ex = -1;
 
         /// update stack info
@@ -588,7 +591,7 @@ int Search::search(int alpha, int beta, int depth, bool cutNode, StackEntry* sta
             }
 
             R += 2 * cutNode;
-            R -= wasPV && ttDepth >= depth;
+            //R -= wasPV && ttDepth >= depth;
 
             R = std::min(newDepth, std::max(R, 1)); /// clamp R
             score = -search<false>(-alpha - 1, -alpha, newDepth - R, true, stack + 1);
