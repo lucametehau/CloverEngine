ddf4d284e64f780a93c75f2f83e3e9dad18f7d9b
lucametehau <31002793+lucametehau@users.noreply.github.com>
2024-07-09 12:50:19 +0200

    Bench: 3122362

--- src/history.h
+++ src/history.h
@@ -105,7 +105,7 @@ void getHistory(Search* searcher, StackEntry* stack, Move move, uint64_t threats
 void updateCorrHist(Search* searcher, int depth, int bonus) {
     int w = std::min(depth + 1, 16);
     int& corr = searcher->corr_hist[searcher->board.turn][searcher->board.pawn_key & 16383];
-    corr = (corr * (CorrHistScale - w) + bonus * w) / CorrHistScale;
+    corr = (corr * (CorrHistScale - w) + bonus * CorrHistDiv * w) / CorrHistScale;
     corr = std::clamp(corr, -16 * CorrHistDiv, 16 * CorrHistDiv);
 }
 
