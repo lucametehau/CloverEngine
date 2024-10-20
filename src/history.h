4aff1c0e6fbb1b1d2d72d18172608fc6eb79a913
lucametehau <31002793+lucametehau@users.noreply.github.com>
2024-10-20 19:07:46 +0200

    Bench: 6491292

--- src/history.h
+++ src/history.h
@@ -179,9 +179,9 @@ public:
     }
 
     inline const int get_corrected_eval(const int eval, const bool turn, const uint64_t pawn_key, const uint64_t white_mat_key, const uint64_t black_mat_key) const {
-        return eval + (get_corr_hist(turn, pawn_key) + 
-                      get_mat_corr_hist(turn, WHITE, white_mat_key) + 
-                      get_mat_corr_hist(turn, BLACK, black_mat_key)) / CorrHistDiv;
+        return eval + (128 * get_corr_hist(turn, pawn_key) + 
+                      64 * get_mat_corr_hist(turn, WHITE, white_mat_key) + 
+                      64 * get_mat_corr_hist(turn, BLACK, black_mat_key)) / (192 * CorrHistDiv);
     }
 };
 
