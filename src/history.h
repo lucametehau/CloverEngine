088d45a6ddc9882dc8afbc19790e5c1e5a2b7fbd
lucametehau <31002793+lucametehau@users.noreply.github.com>
2025-07-17 11:34:02 +0300

    faster
    Bench: 4659381

--- src/history.h
+++ src/history.h
@@ -91,7 +91,7 @@ class Histories
   private:
     MultiArray<History<16384>, 2, 2, 2, 64 * 64> hist;
     MultiArray<History<16384>, 12, 64, 7> cap_hist;
-    MultiArray<History<16384>, 12, 64, PAWN_HIST_SIZE> pawn_hist;
+    MultiArray<History<16384>, PAWN_HIST_SIZE, 12, 64> pawn_hist;
     MultiArray<CorrectionHistory, 2, CORR_HIST_SIZE> corr_hist;
     MultiArray<CorrectionHistory, 2, 2, CORR_HIST_SIZE> mat_corr_hist;
 
@@ -104,7 +104,7 @@ class Histories
     {
         fill_multiarray<History<16384>, 2, 2, 2, 64 * 64>(hist, 0);
         fill_multiarray<History<16384>, 12, 64, 7>(cap_hist, 0);
-        fill_multiarray<History<16384>, 12, 64, PAWN_HIST_SIZE>(pawn_hist, 0);
+        fill_multiarray<History<16384>, PAWN_HIST_SIZE, 12, 64>(pawn_hist, 0);
         fill_multiarray<History<16384>, 2, 13, 64, 13, 64>(cont_history, 0);
         fill_multiarray<CorrectionHistory, 2, CORR_HIST_SIZE>(corr_hist, CorrectionHistory(0));
         fill_multiarray<CorrectionHistory, 2, 2, CORR_HIST_SIZE>(mat_corr_hist, CorrectionHistory(0));
@@ -150,12 +150,12 @@ class Histories
 
     History<16384> &get_pawn_hist(const Piece piece, const Square to, const Key pawn_key)
     {
-        return pawn_hist[piece][to][pawn_key & PAWN_HIST_MASK];
+        return pawn_hist[pawn_key & PAWN_HIST_MASK][piece][to];
     }
 
     const History<16384> get_pawn_hist(const Piece piece, const Square to, const Key pawn_key) const
     {
-        return pawn_hist[piece][to][pawn_key & PAWN_HIST_MASK];
+        return pawn_hist[pawn_key & PAWN_HIST_MASK][piece][to];
     }
 
     CorrectionHistory &get_corr_hist(const bool turn, const Key pawn_key)
