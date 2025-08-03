1135ede827781e287ce9f62c8dde4c3deeaf3d51
lucametehau <31002793+lucametehau@users.noreply.github.com>
2025-08-03 08:59:21 +0300

    Bench: 4706728

--- src/net.h
+++ src/net.h
@@ -97,7 +97,7 @@ INCBIN(Net, EVALFILE);
 
 constexpr int KING_BUCKETS = 1;
 constexpr int INPUT_NEURONS = 768 * KING_BUCKETS;
-constexpr int SIDE_NEURONS = 256;
+constexpr int SIDE_NEURONS = 1024;
 constexpr int HIDDEN_NEURONS = 2 * SIDE_NEURONS;
 constexpr int OUTPUT_NEURONS = 8;
 constexpr int REG_LENGTH = sizeof(reg_type) / sizeof(int16_t);
